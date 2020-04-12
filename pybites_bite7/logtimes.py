535a4374db085a3dcc6efb52547f2942f859d5d2
Greg Williams <greggwilliams58@gmail.com>
2020-04-12 16:28:28 +0100

    completed solution

--- pybites_bite7/logtimes.py
+++ pybites_bite7/logtimes.py
@@ -2,14 +2,13 @@ from datetime import datetime
 import os
 import urllib.request
 
-SHUTDOWN_EVENT = 'Shutdown initiated'
+SHUTDOWN_EVENT = "Shutdown initiated"
 
 # prep: read in the logfile
 tmp = os.getenv("TMP", "/tmp")
-logfile = os.path.join(tmp, 'log')
+logfile = os.path.join(tmp, "log")
 urllib.request.urlretrieve(
-    'https://bites-data.s3.us-east-2.amazonaws.com/messages.log',
-    logfile
+    "https://bites-data.s3.us-east-2.amazonaws.com/messages.log", logfile
 )
 
 with open(logfile) as f:
@@ -17,6 +16,7 @@ with open(logfile) as f:
 
 
 # for you to code:
+
 
 def convert_to_datetime(line):
     """TODO 1:
@@ -26,8 +26,14 @@ def convert_to_datetime(line):
        returns:
        datetime(2014, 7, 3, 23, 27, 51)
     """
-    #test change
-    return line
+    if line[0] == "E":
+        date_object = datetime.strptime(line[6:25], "%Y-%m-%dT%H:%M:%S")
+    elif line[0] == "I":
+        date_object = datetime.strptime(line[5:24], "%Y-%m-%dT%H:%M:%S")
+    else:
+        print("unrecognised format")
+        pass
+    return date_object
 
 
 def time_between_shutdowns(loglines):
@@ -36,4 +42,15 @@ def time_between_shutdowns(loglines):
        calculate the timedelta between the first and last one.
        Return this datetime.timedelta object.
     """
-    pass
+    shutdown_dates = []
+    for line in loglines:
+        if "Shutdown initiated" in line:
+            date = datetime.strptime(line[5:24], "%Y-%m-%dT%H:%M:%S")
+            shutdown_dates.append(date)
+
+    first_date = shutdown_dates[0]
+    last_date = shutdown_dates[-1]
+
+    td_raw = last_date - first_date
+
+    return td_raw
